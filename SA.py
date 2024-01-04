c9d14e3ea2a3e8ad49ac22e7661be151a3ba28fe
cssi87m <daoxuanquangminh2004@gmail.com>
2024-01-05 00:58:56 +0700

    adding comment

--- SA.py
+++ SA.py
@@ -1,10 +1,12 @@
+'''
+Improve greedy algorithm using simulated annealing (SA)
+- Define each state as a topological order of tasks
+- At each state, calculate a solution using greedy algorithm. Based on that solution calculate temperature of the state.
+- Based on SA schema, update the state.
+'''
 import random
 import math 
 from copy import deepcopy
-# '''print all errors in 1 file'''
-# import sys 
-# sys.stderr = open('error.log', 'w') 
-# from CONSTANT import import_data
 class Cycle:
     def __init__(self, tasks):
         self.tasks = tasks
@@ -79,7 +81,12 @@ class Task:
         self.costs = []
         self.time_done = 0
         self.visited = False
-        self.depth = -1
+        self.length = 1
+    def calc_length(self):
+        if self.after_tasks:
+            self.length = max(after_task.calc_length() for after_task in self.after_tasks) 
+            self.length += 1
+        return self.length
 
 class Team:
     def __init__(self, ID):
@@ -147,6 +154,11 @@ class state:
         self.task_done = 0
         self.visited = False
 
+    def __str__(self):
+        res = [str(self.state[i].ID) for i in range(self.n)]
+        return ', '.join(res)
+
+
     def calc_obj(self):
         ''' calculate team order base on state. the team is chosen by greedy algorithm '''
         self.res = []
@@ -175,7 +187,6 @@ class state:
         task_done = sum([(self.team_state[i] > 0) for i in range(self.n)])
         self.task_done = task_done
         # calculate the completion time of all tasks
-        # time_done = self.state[-1].time_done
         time_done = max(self.state, key = lambda x: x.time_done).time_done
         # calculate total cost for task assignment 
         total_cost = 0
@@ -184,9 +195,8 @@ class state:
                 total_cost += self.task_team[self.state[i].ID - 1][self.team_state[i] - 1]
         self.obj = [task_done, time_done, total_cost]
     
-
     def swap(self):
-        # Swap 2 consecutive tasks that still preserve topological order 
+        '''Swap 2 consecutive tasks that still preserve topological order '''
         for i in range(self.n - 1):
             if not self.graph[self.state[i].ID - 1][self.state[i+1].ID - 1]:
                 new_state = state(self.teams, self.task_team, self.state.copy(), self.graph)
@@ -194,8 +204,8 @@ class state:
                 if new_state not in self.neighbour:
                     self.neighbour.append(new_state)
 
-        # Swap 2 consecutive tasks that still preserve topological order 
-        for i in range(self.n - 1, 1, -1):
+        '''Swap 2 consecutive tasks that still preserve topological order '''
+        for i in range(self.n - 1, 0, -1):
             if not self.graph[self.state[i-1].ID - 1][self.state[i].ID - 1]:
                 new_state = state(self.teams, self.task_team, self.state.copy(), self.graph)
                 new_state.state[i], new_state.state[i-1] = new_state.state[i-1], new_state.state[i]
@@ -207,9 +217,10 @@ def is_better(obj1, obj2):
             (obj1[0] == obj2[0] and obj1[1] == obj2[1] and obj1[2] < obj2[2]))
 
 def e_power(obj1, obj2, temperature):
-    return math.exp(((obj1[0] - obj2[0]) - 0.5*(obj1[1] - obj2[1]) - 0.001*(obj1[2] - obj2[2]))/temperature)
+    return math.exp(((obj1[0] - obj2[0]) - 0.8*(obj1[1] - obj2[1]) - 0.001*(obj1[2] - obj2[2]))/temperature)
 
 class SA:
+    LIM = 100
     def __init__(self, temperature, state: state, cooling_factor, num_iterations):
         self.temperature = temperature
         self.state = state
@@ -228,39 +239,52 @@ class SA:
         for _ in range(self.num_iterations):
             '''SA procedure will stop either after self.num_iterations, or best_cand is not changed after an iteration'''
             cand_state = tuple(best_cand.state)
+            i = 0 # count variable 
+            while (i < self.LIM):
+                if self.visited.get(cand_state, 0) == 0:
+                    best_cand.swap()
+                    best_cand = random.choice(best_cand.neighbour)
+                    cand_state = tuple(best_cand.state)
+                i += 1
+            # print(i)
             if self.visited.get(cand_state, 1) == 1:
-                # construct neighborhood of best_cand
+                # print(len(best_cand.neighbour))
                 best_cand.swap()
-                print(len(best_cand.neighbour))
                 self.visited[cand_state] = 0
                 neighbour = best_cand.neighbour
-                for i in range(len(neighbour)):
+                '''Randomly choose 10 neighbour of current best_cand'''
+                for i in range(len(neighbour)//10):
                     # print(f"Checking neighbour at index {i}, length: {len(best_cand.neighbour)}")
-                    candidate = neighbour[i]
+                    candidate = random.choice(neighbour)
                     cand_state = tuple(candidate.state)
                     if self.visited.get(cand_state, 0) == 0:
                         candidate.calc_obj()
                         self.visited[cand_state] = candidate.obj
 
-                    # candidate.obj is better than current obj
+                    '''candidate.obj is better than current obj'''
                     if is_better(self.visited[cand_state], obj):
                         obj = self.visited[cand_state]
+                        '''if obj is better than best_obj, changing the current best candidate'''
                         if is_better(obj, best_obj):
                             best_obj = obj 
                             best_cand = candidate
-                
+
+                        '''if candidate.obj is not better than current obj, calculate p as the probability of going downward.
+                        if p < epsilon, update obj.'''
                     else: 
                         p = min(e_power(obj, self.visited[cand_state], self.temperature), 1)
                         eps = random.random()
                         if eps > p: # downward movement
                             obj = self.visited[cand_state]
+                            # best_cand = cand_state
                         
                         else: 
                             self.cooling_factor *= 1.25
+                    # print(best_obj)
                 self.temperature *= self.cooling_factor
-                # print(best_cand.res)
-            else: 
-                break
+                # print(best_obj)
+            # else: 
+            #     break
         
         # calculate res
         if best_cand.obj == 0: best_cand.calc_obj()
@@ -305,7 +329,6 @@ def import_data(file):
             tasks[b-1].pred_tasks.append(tasks[a-1])
             graph[a-1][b-1] = 1
 
-        
         d = list(map(int, f.readline().split()))
         
         for i in range(n):
@@ -316,6 +339,7 @@ def import_data(file):
         teams = [Team(i+1) for i in range(m)]
         for i in range(m):
             teams[i].avail = s[i]
+            teams[i].last_work = s[i]
             teams[i].cost = [-1 for _ in range(n)]
         
         if len(s) == m: k = int(f.readline())
@@ -339,26 +363,57 @@ def export_data(file, solver):
         for ress in solver.res.res: 
             for i in ress: f.write(str(i)+ " ")
             f.write("\n")
-        f.write(str(solver.res.obj))
+        # f.write(str(solver.res.obj))
 
-def main(): 
-    tasks, teams, task_team, graph = inp()
-    # tasks, teams, task_team, graph = import_data("input.txt")
+def main(input, out, is_print): 
+    # tasks, teams, task_team, graph = inp()
+    if input:
+        tasks, teams, task_team, graph = import_data(input)
+    else: 
+        tasks, teams, task_team, graph = inp()
     # initiate a topo list 
     topo_list = topo(tasks)
+    for task in topo_list:
+        task.length = task.calc_length()
+    topo_list.sort(key = lambda x: -x.length)  
     # initiate params 
     num_iterations = 50
-    temperature = 100 
-    cooling_factor = 0.8
+    temperature = 10
+    cooling_factor = 0.6
     first_state = state(teams, task_team, topo_list, graph)
     solver = SA(temperature, first_state, cooling_factor, num_iterations)
     solver.solve()
-    # print(solver.res.task_done)
-    # for res in solver.res.res:
-    #     for i in res: print(i, end = ' ')
-    #     print()
-    # print(solver.res.obj)
-    export_data("output.txt", solver)
 
-if __name__ == "__main__":
-    main()
+    if is_print:
+        print(solver.res.task_done)
+        for res in solver.res.res:
+            for i in res: print(i, end = ' ')
+            print()
+    else: 
+        export_data(out, solver)
+    print(solver.res.obj)
+    # export_data("output.txt", solver)
+
+local = True
+if local:
+    import os
+    import timeit
+    test_case = -1 
+
+    if test_case == -1: 
+        for t in sorted(os.listdir('input'), key = lambda x: int(x.split(".")[0])):
+            start_time = timeit.default_timer()
+            input = f'input//{t}'
+            out = f'SA//{t}'
+            main(input, out, False)
+            print(f'test case {t}: runtime = {timeit.default_timer() - start_time}')
+
+    else: 
+        start_time = timeit.default_timer()
+        input = f'input//{test_case}.txt'
+        out = f'SA//{test_case}.txt'
+        main(input, out, False)
+        print(f'test case {test_case}: runtime = {timeit.default_timer() - start_time}')
+
+else: 
+    main(False, False, True)
